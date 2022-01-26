d6f995d9051d5ad7d4b5f163c24f9eec62dd6fbe
GallonShih <Gallon.Shih@testritegroup.com>
2022-01-26 09:52:03 +0800

    feet: refactor routes

--- routes/modules/shorts.js
+++ routes/modules/shorts.js
@@ -3,10 +3,14 @@ const router = express.Router()
 const Short = require('../../models/short')
 
 router.post('/', (req, res) => {
-    res.redirect('/shorts/result')
+    const { url_original } = req.body
+    const url_id = '12345'
+    res.redirect(`/shorts/result/${url_id}`)
 })
-router.get('/result', (req, res) => {
-    res.render('result')
+router.get('/result/:url_id', (req, res) => {
+    const url_id = req.params.url_id
+    res.render('result', { url_id })
 })
+
 
 module.exports = router
