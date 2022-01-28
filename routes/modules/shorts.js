3852585e411450d67421ad5782b00b223a771323
GallonShih <Gallon.Shih@testritegroup.com>
2022-01-28 14:38:01 +0800

    feet: check url_original exists or not

--- routes/modules/shorts.js
+++ routes/modules/shorts.js
@@ -5,15 +5,20 @@ const generateShortPath = require('../../generate_short_path')
 
 router.post('/', (req, res) => {
     const { url_original } = req.body
-    const shortPath = generateShortPath()
-    const url_short = `${req.headers.origin}/shorts/links/${shortPath}`
-    const short = new Short({
-        url_original: url_original,
-        url_short: url_short
-    })
-    return short.save()
-        .then((short) => res.redirect(`/shorts/result/${short._id}`))
-        .catch(error => console.log(error))
+    Short.findOne({ url_original: url_original})
+        .lean()
+        .then( (short) => res.redirect(`/shorts/result/${short._id}`))
+        .catch( () => {
+            const shortPath = generateShortPath()
+            const url_short = `${req.headers.origin}/shorts/links/${shortPath}`
+            const short = new Short({
+                url_original: url_original,
+                url_short: url_short
+            })
+            return short.save()
+                .then((short) => res.redirect(`/shorts/result/${short._id}`))
+                .catch(error => console.log(error))
+        })
 })
 router.get('/result/:url_id', (req, res) => {
     const url_id = req.params.url_id
